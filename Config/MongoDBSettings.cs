ca371a305c4a996fda5eade7e9b51197a9ffe14b
Dominat0rr <claermandominik@gmail.com>
2021-11-18 10:45:29 +0100

    Create MongoDBSettings.cs

---
+++ Config/MongoDBSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Config {
+    // Mongo DB Docker command:
+    // docker run -d --rm --name mongo -p 27017:27017 -v mongodbdata:/data/db mongo
+    public class MongoDBSettings {
+        public string host { get; set; }
+        public string port { get; set; }
+
+        public string connectionString { 
+            get {
+                return $"mongodb://{this.host}:{this.port}";
+            }
+        }
+    }
+}
