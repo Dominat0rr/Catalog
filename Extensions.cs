21b7f0a7a82b5da511ccb6670456bb49e5c688b8
Dominat0rr <claermandominik@gmail.com>
2021-11-18 10:45:21 +0100

    Create Extensions.cs

---
+++ Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.DTO;
+using Catalog.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog {
+    public static class Extensions {
+        public static ItemDTO asDTO(this Item item) {
+            return new ItemDTO {
+                id = item.id,
+                name = item.name,
+                price = item.price,
+                created = item.created
+            };
+        }
+    }
+}
