140aad0d6770866404aa6deb775e50306700db2b
Dominat0rr <claermandominik@gmail.com>
2021-11-18 10:44:27 +0100

    Add ItemRepository Interface

---
+++ Repositories/IItemRepository.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.Repositories {
+    public interface IItemRepository {
+        Item getItem(Guid id);
+        Task<Item> getItemAsync(Guid id);
+        IEnumerable<Item> getItems();
+        Task<IEnumerable<Item>> getItemsAsync();
+        void create(Item item);
+        Task createAsync(Item item);
+        void update(Item item);
+        Task updateAsync(Item item);
+        void delete(Guid id);
+        Task deleteAsync(Guid id);
+    }
+}
