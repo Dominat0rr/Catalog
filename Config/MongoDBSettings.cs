d87c8e8224ab18b730fb13965788359a22aadbb3
Dominat0rr <claermandominik@gmail.com>
2021-11-18 15:06:24 +0100

    Update MongoDBSettings.cs

--- Config/MongoDBSettings.cs
+++ Config/MongoDBSettings.cs
@@ -5,14 +5,26 @@ using System.Threading.Tasks;
 
 namespace Catalog.Config {
     // Mongo DB Docker command:
+    // With admin (root) user:
     // docker run -d --rm --name mongo -p 27017:27017 -v mongodbdata:/data/db mongo
+    // With specific user
+    // docker run -d --rm --name mongo -p 27017:27017 -v mongodbdata:/data/db -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=password1 mongo
+    // Run docker image in specific network
+    // docker run -d --rm --name mongo -p 27017:27017 -v mongodbdata:/data/db -e MONGO_INITDB_ROOT_USERNAME = mongoadmin - e MONGO_INITDB_ROOT_PASSWORD=password1 --network=net5tutorial mongo
+
+    // Run docker image with overwriting enviorment variables (we can't connect to mongodb at localhost in the container)
+    // docker run -it -p 8080:80 -e MongoDBSettings:Host=mongo -e MongoDBSettings:Password=password --network=net5tutorial catalog:v1
+
     public class MongoDBSettings {
         public string host { get; set; }
         public string port { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
 
         public string connectionString { 
             get {
                 return $"mongodb://{this.host}:{this.port}";
+                //return $"mongodb://{this.username}:{this.password}@{this.host}:{this.port}";
             }
         }
     }
