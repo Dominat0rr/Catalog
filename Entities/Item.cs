5886fc64299c3cfd8337599f7dd854016cde1f49
Dominat0rr <claermandominik@gmail.com>
2021-11-18 10:43:08 +0100

    Add Item model class

---
+++ Entities/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Entities {
+    // Record Types:
+    // - Use for immutable objects
+    // - With -expressions support
+    // - Value-based equality support
+    public record Item {
+        // init:
+        // - Init-only properties (only can be set during initialization)
+        public Guid id { get; init; }
+        public string name { get; init; }
+        public decimal price { get; init; }
+        public DateTimeOffset created { get; init; }
+
+
+    }
+}
