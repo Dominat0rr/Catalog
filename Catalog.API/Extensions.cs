0227daeb93c5664549864cdaae1687395167d45c
Dominat0rr <claermandominik@gmail.com>
2021-12-06 15:14:13 +0100

    Update Extensions.cs

--- Catalog.API/Extensions.cs
+++ Catalog.API/Extensions.cs
@@ -1,5 +1,4 @@
-﻿using Catalog.API.DTO;
-using Catalog.API.Entities;
+﻿using Catalog.API.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,13 +6,14 @@ using System.Threading.Tasks;
 
 namespace Catalog.API {
     public static class Extensions {
-        public static ItemDTO asDTO(this Item item) {
-            return new ItemDTO {
-                id = item.id,
-                name = item.name,
-                price = item.price,
-                created = item.created
-            };
+        public static ItemDto asDTO(this Item item) {
+            return new ItemDto(
+                item.id, 
+                item.name, 
+                item.description, 
+                item.price, 
+                item.created
+            );
         }
     }
 }
