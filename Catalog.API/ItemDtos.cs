615f485d71d383e313552d8e24d50764aad7214a
Dominat0rr <claermandominik@gmail.com>
2021-12-06 15:14:04 +0100

    Create ItemDtos.cs

---
+++ Catalog.API/ItemDtos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API {
+    public record ItemDto(Guid id, string name, string description, decimal price, DateTimeOffset created);
+    public record CreateItemDto([Required]string name, string description, [Range(1, 1000)]decimal price);
+    public record UpdateItemDto([Required] string name, string description, [Range(1, 1000)] decimal price);
+}
